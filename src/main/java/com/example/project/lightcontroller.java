e0455e2182837a683968ff3926d03a4fcb86db69
flyingstopsign <Epicbrady101@gmail.com>
2025-04-21 22:44:36 +0000

    code: added light controller variable

--- src/main/java/com/example/project/lightcontroller.java
+++ src/main/java/com/example/project/lightcontroller.java
@@ -3,7 +3,9 @@ package com.example.project;
 public class lightcontroller {
     private boolean [][] lights;
 
-
+    public lightcontroller(int size){
+        lights = new boolean [size][size];
     }
 
 }
+
